bf6c96d9bad5a83b3e10c0a821f5ec39b57eaa1b
REAL-FD2 <wasimchy100@gmail.com>
2020-06-02 18:26:57 +0100

    slap command changes

--- source/Commands/SlapCommand.ts
+++ source/Commands/SlapCommand.ts
@@ -11,13 +11,13 @@ export class SlapCommand extends AbstractCommand
 
     public async runInternal(bot: Client, message: Message, messageArray: string[]) 
     {
-        let targetUser: GuildMember = message.mentions.members.first();
-        const randomCooldownMessage = ["You seem to be really mad today. Calm down a bit", "That's it, I'm calling the police."];
-        if(targetUser)
+        let targetMember: GuildMember = message.mentions.members.first();
+        const randomCooldownMessage: Array<String> = ["You seem to be really mad today. Calm down a bit", "That's it, I'm calling the police."];
+        if(targetMember)
         {
             if(this.cooldownService.isCooldown(message.member, this.commandOptions.commandName + "<functional")) return message.channel.send(randomCooldownMessage[Math.floor(Math.random() * 2)]);
             else {
-                if(targetUser == message.member) return message.channel.send("Stop hating yourself. You don't deserve that.");
+                if(targetMember == message.member) return message.channel.send("Stop hating yourself. You don't deserve that.");
                 let userToSlap: User = await this.userService.getUser(message.mentions.members.first(), message.guild);
             
                 if(userToSlap.headPats === 0) {
