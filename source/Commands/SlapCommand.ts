e630c4c94bbff6f0bcd4fec2df9400cca71ebb11
Wasim Choudhury <64626148+REAL-FD2@users.noreply.github.com>
2020-06-17 20:56:35 +0100

    Update SlapCommand.ts

--- source/Commands/SlapCommand.ts
+++ source/Commands/SlapCommand.ts
@@ -1,5 +1,5 @@
 import { AbstractCommand } from "./AbstractCommand";
-import {Client, Message, GuildMember} from 'discord.js';
+import {Client, Message, GuildMember, MessageEmbed} from 'discord.js';
 import * as Discord from 'discord.js';
 import { UserService } from "../Service/UserService";
 import {User} from "../Material/User";
@@ -18,28 +18,48 @@ export class SlapCommand extends AbstractCommand
         if(targetMember)
         {
             if(this.cooldownService.isCooldown(message.member, this.commandOptions.commandName + "<functional")) 
-                return message.channel.send(randomCooldownMessage[Math.floor(Math.random() * 2)]);
+                return message.channel.send(new MessageEmbed().setColor("#ff0000").setDescription(randomCooldownMessage[Math.floor(Math.random() * 2)]));
             else 
             {
-                if(targetMember == message.member) return message.channel.send("Stop hating yourself. You don't deserve that.");
+                if(targetMember == message.member) return message.channel.send(super.getFailedEmbed().setDescription("Stop hating yourself. You don't deserve that."));
                 let userToSlap: User = await this.userService.getUser(targetMember, message.guild);
             
                 if(userToSlap.headPats === 0) 
                 {
-                    message.channel.send("Looks like they have no headpats left.")
+                    message.channel.send(super.getFailedEmbed().setDescription("Looks like they have no headpats left."))
                 } 
                 else 
                 {
-                    userToSlap.headPats --;
-                    userToSlap.save();  
-                    message.channel.send(`${message.member} slapped ${targetMember}`);
-                    this.cooldownService.addCooldown(message.member, this.commandOptions.commandName + "<functional", 7200);  
+                    let randomInt = Math.floor(Math.random() * 3); 
+                    if(randomInt === 1)
+                    {
+                        if((await this.userService.getUser(targetMember, message.guild)).permissionLevel >= PermissionLevel.master)
+                        {
+                            let userToSlap: User = await this.userService.getUser(message.member, message.guild);
+                            message.channel.send(new MessageEmbed().setColor("#ff0000").setDescription(`How dare you slap the ones creating me? \n Get a taste of your own medicine.`))
+                            message.channel.send(new MessageEmbed().setColor("#00FF00").setDescription(`*Bottius slapped ${message.member}*`))
+                            if(userToSlap.headPats != 0)
+                            {
+                                userToSlap.headPats --;
+                                userToSlap.save();    
+                            }
+                            this.cooldownService.addCooldown(message.member, this.commandOptions.commandName + "<functional", 7200); 
+                        }                        
+                    }
+                    else
+                    {
+                        userToSlap.headPats --;
+                        userToSlap.save();  
+                        message.channel.send(new MessageEmbed().setColor("#00FF00").setDescription(`${message.member} slapped ${targetMember}`));
+                        this.cooldownService.addCooldown(message.member, this.commandOptions.commandName + "<functional", 7200);  
+                    }
+
                 }
             }
         }
         else
         {
-            message.channel.send("Specify a user to slap")
+            message.channel.send(super.getFailedEmbed().setDescription("Specify a user to slap"))
         }
 
     }
