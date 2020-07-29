aea9ccf6b098cf08e603e22c2171b02a6d40bf88
Wasim Choudhury <64626148+REAL-FD2@users.noreply.github.com>
2020-07-29 22:25:10 +0100

    Activity is persistent (pt. 2) (#149)

--- source/bot.ts
+++ source/bot.ts
@@ -1,4 +1,4 @@
-import {Client, MessageEmbed, TextChannel} from 'discord.js';
+import {Client, MessageEmbed, TextChannel, Role} from 'discord.js';
 import { CommandService } from './Service/CommandService';
 import botConfig from "./botconfig.json";
 import { MessageService } from './Service/MessageService';
@@ -37,8 +37,10 @@ bot.on("ready", async () =>
    setInterval(() => delayedTaskService.handleDueDelayedTasks(), 600000);
    setInterval(() => tumbleWeedService.handleTumbleWeed(bot), 600000);
    console.log("INFO: All services loaded. Bot is ready.")
-   
-   //Reimplement activity set. It isnt working rn anyways so I just kicked it out.
+
+   const fs = require('fs');
+   let config = JSON.parse(fs.readFileSync('botconfig.json', 'utf8'))
+   bot.user.setActivity(config.activity, {type : config.activityStatus, url : "https://twitch.tv/smexy-briccs"});
 });
 
 bot.on("message", async message =>
