ea7b2197fcb3ecc28b64b2ec07854ba1de2fb322
StrangeRanger <strangeranger01@gmail.com>
2020-08-03 15:10:50 -0800

    Installer progress around 98%

--- installers/Linux_Universal/postgres-open-close.sh
+++ installers/Linux_Universal/postgres-open-close.sh
@@ -11,6 +11,9 @@
 ################################################################################
 #
     clear
+    pg_hba="/var/lib/pgsql/12/data/pg_hba.conf"
+    gres="/var/lib/pgsql/12/data/postgresql.conf"
+
 
 #
 ################################################################################
@@ -20,7 +23,7 @@
 ################################################################################
 #
     are_you_sure() {
-        verify_choice=$((1 + RANDOM % 10))$((1 + RANDOM % 10))$((1 + RANDOM % 10))
+        verify_choice=$((RANDOM % 10))$((RANDOM % 10))$((RANDOM % 10))
         echo "${yellow}WARNING: By choosing this option and opening Postgres" \
             "to the outside world, you will be allowing users outside the" \
             "server attempt to connect to the database. So if you have not" \
@@ -31,7 +34,6 @@
         read -p "Please enter the number above, here: " number
 
         if [[ $number = $verify_choice ]]; then
-            read -p "Press enter to continue and open Postgres to the world"
             return 0
         else
             echo "${red}The numbers do not match${nc}"
@@ -46,19 +48,26 @@
                 clear
                 echo "${yellow}Already open to outside of the server${nc}"
                 return 1
+            else
+                return 0
             fi
         elif [[ $pgsql_status = "close" ]]; then
             if [[ $1 = "open" ]]; then
-                are_you_sure || clear && return 1
+                are_you_sure && return 0 || clear && return 1
             else
                 clear
                 echo "${yellow}Already closed to outside of the server${nc}"
                 return 1
             fi
         elif [[ $pgsql_status = "partial" ]]; then
-            if [[ $1 = "open" ]]; then are_you_sure || clear && return 1; fi
+            if [[ $1 = "open" ]]; then 
+                are_you_sure && return 0 || clear && return 1
+            else
+                return 0
+            fi
         elif [[ $pgsql_status = "unkown" ]]; then
             echo "${yellow}Due to the unkown status, there will be no changes${nc}"
+            return 1
         fi
     }
 
@@ -70,11 +79,17 @@
 ################################################################################
 #
     while true; do
-        pgsql_hba_opened=$(grep -P "^host .* all .* all .* 0.0.0.0\/0 .* (md5|ident)$" /var/lib/pgsql/12/data/pg_hba.conf)
-        pgsql_gres_opened=$(grep -P "^listen_addresses = '\*'" /var/lib/pgsql/12/data/postgresql.conf)
-        pqsql_hba_closed=$(grep -P "^host .* all .* all .* (127.0.0.1(\/([0-9]|[1-2][0-9]|3[0-2]))?|localhost) .* (ident|md5)$" /var/lib/pgsql/12/data/pg_hba.conf)
-        pgsql_gres_closed=$(grep -P "^.?listen_addresses = ('127.0.0.1(\/([0-9]|[1-2][0-9]|3[0-2]))?'|'localhost')" /var/lib/pgsql/12/data/postgresql.conf)
-        pgsql_gres_closed2=$(grep -P "^#listen_addresses = '.*'" /var/lib/pgsql/12/data/postgresql.conf)
+        hba_open="^host .* all .* all .* 0.0.0.0\/0 .* (md5|ident)$"
+        gres_open="^listen_addresses = '\*'"
+        hba_close="^host .* all .* all .* (127.0.0.1(\/([0-9]|[1-2][0-9]|3[0-2]))?|localhost) .* (ident|md5)$"
+        gres_close="^.?listen_addresses = ('127.0.0.1(\/([0-9]|[1-2][0-9]|3[0-2]))?'|'localhost')"
+        gres_close2="^#listen_addresses = '.*'"
+
+        pgsql_hba_opened=$(grep -P "$hba_open" "$pg_hba")
+        pgsql_gres_opened=$(grep -P "$gres_open" "$gres")
+        pgsql_hba_closed=$(grep -P "$hba_close" "$pg_hba")
+        pgsql_gres_closed=$(grep -P "$gres_close" "$gres")
+        pgsql_gres_closed2=$(grep -P "$gres_close2" "$gres")
 
         echo "Advanced Options Menu"
 
@@ -82,12 +97,12 @@
             echo -e "Is Postgres database accessible from outside of the server:" \
                 "${cyan}Yes\n${nc}"
             pgsql_status="open"
-        elif [[ $pqsql_hba_closed && ($pgsql_gres_closed || $pgsql_gres_closed2) ]]; then
+        elif [[ $pgsql_hba_closed && ($pgsql_gres_closed || $pgsql_gres_closed2) ]]; then
             echo -e "Is Postgres database accessible from outside of the server:" \
                 "${cyan}No\n${nc}"
             pgsql_status="close"
         elif [[ $pgsql_hba_opened && ($pgsql_gres_closed || $pgsql_gres_closed2) ||
-                $pqsql_hba_closed && $pgsql_gres_opened ]]; then
+                $pgsql_hba_closed && $pgsql_gres_opened ]]; then
             echo -e "Is Postgres database accessible from outside of the server:" \
                 "${cyan}Partially\n${nc}"
             pgsql_status="partial"
@@ -105,12 +120,62 @@
             1)
                 clear
                 pgsql_status_check "open" || continue
+                clear
+                read -p "Press [Enter] to continue and open Postgres to the world"
+
+                echo "Modifying '$pg_hba'..."
+                if [[ $pgsql_hba_closed ]]; then
+                    sed -i "s/^host .* all .* all .* \(127.0.0.1\(\/\([0-9]\|[1-2][0-9]\|3[0-2]\)\)\?\|localhost\) .* \(ident\|md5\)$/host   all   all   0.0.0.0\/0   md5/g" "$pg_hba" || {
+                        echo "${red}Failed to modify $pg_hba$nc" >&2
+                    }
+                else
+                    echo "Already set to open"
+                fi
+
+                echo "Modifying '$gres'..."
+                if [[ ($pgsql_gres_closed || $pgsql_gres_closed2) ]]; then
+                    if [[ $pgsql_gres_closed ]]; then
+                        sed -i "s/$gres_close/listen_addresses = '\*'/g" "$gres" || {
+                            echo "${red}Failed to modify $gres$nc" >&2
+                        }
+                    else
+                        sed -i "s/$gres_close2/listen_addresses = '\*'/g" "$gres" || {
+                            echo "${red}Failed to modify $gres$nc" >&2
+                        }
+                    fi
+                else
+                    echo "Already set to open"
+                fi
+
                 read -p "Press [Enter] to return to the Advanced Options Menu"
+                clear
                 ;;
             2)
                 clear
                 pgsql_status_check "close" || continue
+                clear
+                read -p "Press [Enter] to continue and close Postgres to the world"
+
+                echo "Modifying '$pg_hba'..."
+                if [[ $pgsql_hba_closed ]]; then
+                    echo "Already set to close"
+                else
+                    sed -i "s/^host .* all .* all .* 0.0.0.0\/0 .* \(md5\|ident\)$/host   all   all   127.0.0.1\/32   md5/g" "$pg_hba" || {
+                        echo "${red}Failed to modify $pg_hba$nc" >&2
+                    }
+                fi
+
+                echo "Modifying '$gres'..."
+                if [[ ($pgsql_gres_closed || $pgsql_gres_closed2) ]]; then
+                    echo "Already set to close"
+                else
+                    sed -i "s/$gres_open/#listen_addresses = '\*'/g" "$gres" || {
+                        echo "${red}Failed to modify $gres$nc" >&2
+                    }
+                fi
+
                 read -p "Press [Enter] to return to the Advanced Options Menu"
+                clear
                 ;;
             3)
                 echo -e "\nReturning to the installer menu..."
@@ -120,6 +185,5 @@
                 clear
                 echo "${red}Invalid input: '$option' is not a valid" \
                     "option${nc}" >&2
-                continue
         esac
     done
