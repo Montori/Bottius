6729f5a211c7fc3db8e284218a0800e32f7136d0
Montori <malte.bohn@voidtech.de>
2020-08-16 12:37:41 +0200

    Users should be able to see perk list #163

--- source/Commands/PerkCommand.ts
+++ source/Commands/PerkCommand.ts
@@ -4,16 +4,21 @@ import { Client, Message, Role, MessageEmbed } from "discord.js";
 import { Perk } from "../Entities/Persistent/Perk";
 import { PerkService } from "../Service/PerkService";
 import { PermissionLevel } from "../Entities/Transient/PermissionLevel";
+import { User } from "../Entities/Persistent/User";
 
 export class PerkCommand extends AbstractCommand
 {
     public commandOptions: AbstractCommandOptions = new PerkCommandOptions();
     private perkService: PerkService = PerkService.getInstance();
-
+    
     public async runInternal(bot: Client, message: Message, messageArray: string[]) 
     {
+        let exeUser: User = await this.userService.getUser(message.member, message.guild);
+        
         if(messageArray[0] == "add")
         {
+            if(exeUser.permissionLevel < PermissionLevel.admin) return super.sendPermissionDenied(message, PermissionLevel.admin);
+
             messageArray = messageArray.slice(1);
             if(!messageArray[0]) return super.sendHelp(message);
 
@@ -32,10 +37,12 @@ export class PerkCommand extends AbstractCommand
                                         .addField("Level", `${level}`, true)
                                         .addField("Role", `${role}`, true)
                                         .setColor(role.color);
-            message.channel.send(embed);
+            return message.channel.send(embed);
         }
         else if(messageArray[0] == "remove")
         {
+            if(exeUser.permissionLevel < PermissionLevel.admin) return super.sendPermissionDenied(message, PermissionLevel.admin);
+
             let role: Role = message.mentions.roles.first();
             if(!role) return super.sendHelp(message);
             if(! await this.perkService.doesPerkExist(role.id)) return message.channel.send(new MessageEmbed().setAuthor("Perk not existent").setColor("ff0000").setDescription("There is no perk with this role"));
@@ -46,13 +53,14 @@ export class PerkCommand extends AbstractCommand
                                         .setDescription(`A perk has been removed`)
                                         .addField("Role", `${role}`, true);
 
-            message.channel.send(embed);
+            return message.channel.send(embed);
         }
-        else if(messageArray[0] == "list")
+        if(messageArray[0] == "list")
         {
             let perks: Array<Perk> = await this.perkService.getAllPerks(message.guild);
-
             let embed: MessageEmbed = super.getSuccessEmbed("Perks of " + message.guild.name);
+
+            if(!perks.length) embed.setDescription("There are currently no perks configured for this server");
 
             for(const perk of perks)
             {
@@ -60,12 +68,10 @@ export class PerkCommand extends AbstractCommand
                 embed.addField(`Level: ${perk.reqLevel}`, `${role}`)
             }
                 
-            message.channel.send(embed);
+            return message.channel.send(embed);
         }
-        else
-        {
-            super.sendHelp(message);
-        }
+        
+        super.sendHelp(message);
     }
 
 }
@@ -78,6 +84,5 @@ class PerkCommandOptions extends AbstractCommandOptions
         this.commandName = "perk";
         this.description = "adds, removes or lists leveling rewards";
         this.usage = `${AbstractCommandOptions.prefix}perk add {lvl} {@role}\n${AbstractCommandOptions.prefix}perk remove {role}\n${AbstractCommandOptions.prefix}perk list`;
-        this.reqPermission = PermissionLevel.admin;
     }
 }
